c26f11545a992df40d757ac17f3c87659c06f2ca
jpCoding003 <jpcoding003@gmail.com>
2025-08-06 22:44:42 +0530

    Import Image from Camera..

--- app/src/main/java/com/tops/gallery/NewActivity.kt
+++ app/src/main/java/com/tops/gallery/NewActivity.kt
@@ -1,6 +1,7 @@
 package com.tops.gallery
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
@@ -8,15 +9,30 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.tops.gallery.databinding.ActivityNewBinding
+import java.io.File
 
 class NewActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityNewBinding
 
+    // Pic Image from Gallery
     private lateinit var pickMedia: ActivityResultLauncher<PickVisualMediaRequest>
+
+
+    //Take Image By Camera
+    private lateinit var photoUri: Uri
+    val takeCameraPick = registerForActivityResult(ActivityResultContracts.TakePicture()) { sucess ->
+        if (sucess) {
+            Log.d("PicturePicker", "Selected URI: $sucess")
+            binding.cameraimgview.setImageURI(photoUri)
+        } else {
+            Log.d("camera", "Image capture cancelled")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +46,13 @@ class NewActivity : AppCompatActivity() {
         }
 
         binding.btnGallery.setOnClickListener {
+            // Pic Image from Gallery
             pickImageFromGallery()
+        }
+
+        binding.btnCamera.setOnClickListener {
+            //Take Image By Camera
+            CaptureImageFromCamera()
         }
 
         pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
@@ -42,12 +64,34 @@ class NewActivity : AppCompatActivity() {
             }
         }
 
+
+
         binding.btnBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
+
+
+    // Pic Image from Gallery
     private fun pickImageFromGallery() {
         pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+
+    //Take Image By Camera
+    private fun CaptureImageFromCamera() {
+        //Create temporary file
+        val imageFile = File.createTempFile("IMG_", ".jpg", cacheDir)
+
+        //Get content URI using FileProvider
+        photoUri = FileProvider.getUriForFile(
+            this,
+            "${packageName}.provider",
+            imageFile
+        )
+
+        //Launch the camera
+        takeCameraPick.launch(photoUri)
     }
 }
